8a00afef9645440f298e5450ce1020f924fe126a
luizbaidum <luiz.baidum@hotmail.com.br>
2022-09-12 20:31:43 -0300

    commit de segurança
    - testar try catch

---
+++ public/scripts/crud-operations.js
@@ -0,0 +1,13 @@
+$('#salvar').on('click', (e) => {
+
+    e.preventDefault();
+
+    let dados = $('form').serialize();
+
+    //ver como isso vai ficar
+    //como criarei os objetos no php
+
+
+
+    console.log(teste);
+})
